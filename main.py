08e3a3ab6ebbb81b325e34db87897ff9d16388dd
liaohaha <645741292@qq.com>
2023-07-02 21:49:03 +0800

    collections 常用

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
+from collections import Counter
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# 创建一个Counter对象
+c = Counter(['a', 'b', 'a', 'c', 'b', 'a'])
 
+# 统计元素的出现次数
+print(c)  # 输出: Counter({'a': 3, 'b': 2, 'c': 1})
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# 访问元素的出现次数
+print(c['a'])  # 输出: 3
+print(c['d'])  # 输出: 0
 
+from collections import deque
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# 创建一个deque对象
+d = deque([1, 2, 3])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 在右端添加元素
+d.append(4)
+print(d)  # 输出: deque([1, 2, 3, 4])
+
+# 在左端添加元素
+d.appendleft(0)
+print(d)  # 输出: deque([0, 1, 2, 3, 4])
+
+# 从右端删除元素
+d.pop()
+print(d)  # 输出: deque([0, 1, 2, 3])
+
+# 从左端删除元素
+d.popleft()
+print(d)  # 输出: deque([1, 2, 3])
+
+from collections import namedtuple
+
+# 创建一个namedtuple类
+Point = namedtuple('Point', ['x', 'y'])
+
+# 创建一个Point对象
+p = Point(1, 2)
+
+# 访问字段值
+print(p.x)  # 输出: 1
+print(p.y)  # 输出: 2
+
+from collections import defaultdict
+
+# 创建一个默认值为0的defaultdict
+d = defaultdict(int)
+
+# 访问不存在的键，返回默认值0，并将该键添加到字典中
+print(d['a'])  # 输出: 0
